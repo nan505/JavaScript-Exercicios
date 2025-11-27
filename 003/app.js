301a8af8911475b998d2743476f78b002d03ea86
Nan Santos <pixelado405@gmail.com>
2025-11-27 00:46:40 -0300

    Exercício 3 resolvido! (Aparentemente)

---
+++ 003/app.js
@@ -0,0 +1,19 @@
+function fnRelogio(){
+    let data = new Date(), horas = data.getHours().toString().padStart(2, "0"), minutos = data.getMinutes().toString().padStart(2, "0"), msgHora = document.getElementById("msgHora")
+
+    document.getElementById("horaAtual").innerText = `${horas}:${minutos}`
+    if(horas >= 6 && horas < 12){
+        msgHora.innerText = "Bom dia!"
+    }
+    else if(horas >= 12 && horas < 18){
+        msgHora.innerText = "Boa tarde!"
+    }
+    else if(horas >= 18 && horas > 0){
+        msgHora.innerText = "Boa noite!"
+    }
+    else{
+        msgHora.innerText = "Boa madrugada!"
+    }
+}
+
+setInterval(fnRelogio, 1000)
